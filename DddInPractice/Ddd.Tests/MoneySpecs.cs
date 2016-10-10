ed872cc6014af9f6b63cbf252064f8ef9d494153
neil-thompson[2] <neil@treven-barn.co.uk>
2016-10-10 18:04:02 +0100

    add tests

---
+++ DddInPractice/Ddd.Tests/MoneySpecs.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ddd.Logic;
+using FluentAssertions;
+using Xunit;
+
+namespace Ddd.Tests
+{
+    public class MoneySpecs
+    {
+        [Fact]
+        public void Sum_of_two_moneys_produces_correct_result()
+        {
+            Money money1 = new Money(1, 2, 3, 4, 5, 6);
+            Money money2 = new Money(1, 2, 3, 4, 5, 6);
+
+            Money sum = money1 + money2;
+
+            Assert.Equal(2, sum.OneCentCount); //we expect the sum of the 1 cent count to be 2. 
+
+            //but lets use fluent assertions instead
+            sum.OneCentCount.Should().Be(2);
+            sum.TenCentCount.Should().Be(4);
+            sum.QuarterCount.Should().Be(6);
+            sum.OneDollarCount.Should().Be(8);
+            sum.FiveDollarCount.Should().Be(10);
+            sum.TwentyDollarCount.Should().Be(12);
+        }
+
+        [Fact]
+        public void Two_money_instances_equal_if_contain_the_same_money_amount()
+        {
+            Money money1 = new Money(1, 2, 3, 4, 5, 6);
+            Money money2 = new Money(1, 2, 3, 4, 5, 6);
+
+            money1.Should().Be(money2);
+            money1.GetHashCode().Should().Be(money2.GetHashCode());
+        }
+
+        [Fact]
+        public void Two_money_instances_dontequal_if_contain_different_money_amount()
+        {
+            Money dollar = new Money(0, 0, 0, 1, 0, 0);
+            Money hundredCents = new Money(100, 0, 0, 0, 0, 0);
+
+            dollar.Should().NotBe(hundredCents);
+            dollar.GetHashCode().Should().NotBe(hundredCents.GetHashCode());
+            //even though they have the same monatary worth they should not be considered equal.
+        }
+
+        [Theory]
+        [InlineData(-1, 0, 0, 0, 0, 0)]
+        [InlineData(0, -2, 0, 0, 0, 0)]
+        [InlineData(0, 0, -3, 0, 0, 0)]
+        [InlineData(0, 0, 0, -4, 0, 0)]
+        [InlineData(0, 0, 0, 0, -5, 0)]
+        [InlineData(0, 0, 0, 0, 0, -6)]
+        public void Cannot_create_money_with_negative_value(int oneCentCount, int tenCentCount, int quarterCount, int oneDollarCount, int fiveDollarCount, int twentyDollarCount)
+        {
+            // Money money = new Money( oneCentCount,  tenCentCount,  quarterCount,  oneDollarCount,  fiveDollarCount,  twentyDollarCount);
+            Action action = () => new Money(oneCentCount, tenCentCount, quarterCount, oneDollarCount, fiveDollarCount, twentyDollarCount);
+            action.ShouldThrow<InvalidOperationException>();
+        }
+
+    }
+}
