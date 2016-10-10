d9954b6541b17d792860df49346b5c6dad832904
neil-thompson[2] <neil@treven-barn.co.uk>
2016-10-10 13:04:46 +0100

    add new solution and starter class for SnackMachine
    also gitignore

---
+++ DddInPractice/Ddd.Logic/SnackMachine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ddd.Logic
+{
+    public sealed class SnackMachine
+    {
+        //sealed - least few privilege.
+
+        public int OneCentCount { get; private set; }
+        public int TenCentCount { get; private set; }
+        public int QuarterCount { get; private set; }
+        public int OneDollarCount { get; private set; }
+        public int FiveDollarCount { get; private set; }
+        public int TwentyDollarCount { get; private set; }
+
+    }
+}
